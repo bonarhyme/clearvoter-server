4e09bbedd9c04c7d357f6935ce0d0429433d8ecd
Bonaventure Chukwudi <bonarhyme@gmail.com>
2022-06-28 23:06:43 +0100

    Disabled email sending after registration

--- controllers/user.controllers.js
+++ controllers/user.controllers.js
@@ -2,8 +2,8 @@ const asyncHandler = require("express-async-handler");
 const UserModel = require("../models/user.model");
 const { successResponse } = require("../utilities/handleResponse");
 const tokenHandler = require("../utilities/handleToken");
-const mailHandler = require("../utilities/mail/handleMail");
-const CLIENT_URL = process.env.CLIENT_URL;
+// const mailHandler = require("../utilities/mail/handleMail");
+// const CLIENT_URL = process.env.CLIENT_URL;
 
 const user = {};
 
@@ -34,16 +34,16 @@ user.register = asyncHandler(async (req, res) => {
     });
 
     if (newUser) {
-      const token = tokenHandler.generateToken(email, "1d");
-      mailHandler.sendMessage({
-        mailExplainer:
-          "You have received this message because your email address has been registered with our site.",
-        mailPrompt:
-          "Please click the button below to verify your email address and confirm that you are the owner of this account.",
-        url: CLIENT_URL + "/verify/" + token,
-        buttonText: "Verify Email",
-        subject: "ClearVoter: Verify your email",
-      });
+      // const token = tokenHandler.generateToken(email, "1d");
+      // mailHandler.sendMessage({
+      //   mailExplainer:
+      //     "You have received this message because your email address has been registered with our site.",
+      //   mailPrompt:
+      //     "Please click the button below to verify your email address and confirm that you are the owner of this account.",
+      //   url: CLIENT_URL + "/verify/" + token,
+      //   buttonText: "Verify Email",
+      //   subject: "ClearVoter: Verify your email",
+      // });
       successResponse(res, 201, "Account created successfully.");
     }
   } catch (error) {
